6c390b992cee5370fa728208b68a502f6f5e75c8
Ali Asad <ali.soft@live.com>
2016-07-19 03:03:08 +0500

    Project Files

---
+++ 4 - Get Started With Solution Explorer & Learn Basic Work Flow - C# Tutorial For Beginners - Ali Asad/BasicFlow/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace BasicFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Run();
+            Run();
+
+            Console.ReadLine();
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Run robot Run");
+        }
+
+
+    }
+
+    class Computer
+    {
+
+    }
+
+    class Enemy
+    {
+
+    }
+}
---
+++ 5 - Variable & Data Types In C#  - C# Tutorial For Beginners - Ali Asad/variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 12;
+            int b = 10;
+
+            Console.WriteLine("a + b = {0} a-b = {1} a*b = {2}", (a + b), (a - b), (a * b));
+
+            Console.ReadLine();
+        }
+    }
+}
