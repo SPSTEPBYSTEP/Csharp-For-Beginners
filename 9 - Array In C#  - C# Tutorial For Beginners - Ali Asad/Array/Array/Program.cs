ef0d2f74174248b5e11980f1d5f11baa3c0d56c2
RAO NISAR AHMED <m.nisar@live.com>
2017-11-07 17:21:49 +0500

    Array Compleated
    All different  code is written in different region.

---
+++ 9 - Array In C#  - C# Tutorial For Beginners - Ali Asad/Array/Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region simple array
+
+            //int[] myarray = { 0, 1, 2, 3, 4, 5 };
+            //Console.WriteLine(myarray[3]);
+            //Console.ReadLine();
+            #endregion
+
+            #region get values from user
+            int[] myarray = new int[5];
+
+            myarray[0] = 1;
+            myarray[1] = 2;
+            myarray[2] = 3;
+            Console.WriteLine(myarray[2]);
+            Console.ReadLine();
+            #endregion
+
+        }
+    }
+}
