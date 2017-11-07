a4e1acc1352e1cb1640f52684b776339f3c0a13e
RAO NISAR AHMED <m.nisar@live.com>
2017-11-07 18:04:47 +0500

    While Loop compleated

---
+++ 11 - While loop In C#  - C# Tutorial For Beginners - Ali Asad/While loop/While loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int check = 1;
+            while (check<=5)
+            {
+                Console.WriteLine("Nisar Ahmed");
+                check++;
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
