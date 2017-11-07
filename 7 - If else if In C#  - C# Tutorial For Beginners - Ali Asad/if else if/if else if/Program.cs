2de90903a0d9248b2f349182f51f3318b22077e6
RAO NISAR AHMED <m.nisar@live.com>
2017-11-07 16:49:38 +0500

    if else if compleated
    All different code is written in different region

---
+++ 7 - If else if In C#  - C# Tutorial For Beginners - Ali Asad/if else if/if else if/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region not optamised way
+
+            //int Marks=55;
+            //string Grade = "";
+
+            //if (Marks > 40 && Marks < 50)
+            //{
+            //    Grade = "c";
+            //}
+            //if (Marks>50 && Marks <60)
+            //{
+            //    Grade = "B";
+            //}
+            //if (Marks>60 && Marks < 70)
+            //{
+            //    Grade = "A";
+            //}
+            //if (Marks >80 && Marks <100)
+            //{
+            //    Grade = "A+";
+            //}
+            //if (Marks<40)
+            //{
+            //    Grade = "F";
+            //}
+
+            Console.WriteLine("Your Grade is : {0}",Grade);
+            #endregion
+
+
+            #region  optamised way
+
+           // int Marks = 55;
+           // string Grade = "";
+
+           // if (Marks > 40 && Marks < 50)
+           // {
+           //     Grade = "c";
+           // }
+           //else if (Marks > 50 && Marks < 60)
+           // {
+           //     Grade = "B";
+           // }
+           //else if (Marks > 60 && Marks < 70)
+           // {
+           //     Grade = "A";
+           // }
+           //else if (Marks > 80 && Marks < 100)
+           // {
+           //     Grade = "A+";
+           // }
+           //else if (Marks < 40)
+           // {
+           //     Grade = "F";
+           // }
+
+           // Console.WriteLine("Your Grade is : {0}", Grade);
+            #endregion
+
+
+        }
+    }
+}
