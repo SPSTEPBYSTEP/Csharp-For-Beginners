3aadd565734c025e50a812c018ef48b75fedbe3e
RAO NISAR AHMED <m.nisar@live.com>
2017-11-07 17:07:21 +0500

    Conditional operator compleate
    All different code is written in different region

---
+++ 8 - Conditional Operator In C#  - C# Tutorial For Beginners - Ali Asad/Conditional Oper/Conditional Oper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Oper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region simple if else
+            //string message;
+            //if (2>4)
+            //{
+            //    message = "2 is less then 4";
+            //}
+            //else
+            //{
+            //    message = "2 is grater then 4";
+            //}
+
+            //Console.WriteLine(message);
+            #endregion
+
+            #region conditional operater
+
+            string message = (2 > 4) ? "2 is grater then 4" : "2 is less then 4";
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+            #endregion
+        }
+    }
+}
