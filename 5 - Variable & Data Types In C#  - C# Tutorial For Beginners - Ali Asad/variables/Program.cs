f63b870282025918078c8bae11de6ca0014179e7
RAO NISAR AHMED <m.nisar@live.com>
2017-11-07 13:04:04 +0500

    compleated
    All different code has been written in different regions'

--- 5 - Variable & Data Types In C#  - C# Tutorial For Beginners - Ali Asad/variables/Program.cs
+++ 5 - Variable & Data Types In C#  - C# Tutorial For Beginners - Ali Asad/variables/Program.cs
@@ -10,13 +10,109 @@ namespace variables
     {
         static void Main(string[] args)
         {
+            #region use of int
 
-            int a = 12;
-            int b = 10;
+            //int age;
+            //age=22
+            //Console.WriteLine(age);
+            //Console.ReadLine();
+            #endregion
+            //==================================================>>
+            #region use of int with incriment
+            //int age;
+            //age = 22;
+            //age++;
+            //Console.WriteLine(age);
+            //Console.ReadLine();
 
-            Console.WriteLine("a + b = {0} a-b = {1} a*b = {2}", (a + b), (a - b), (a * b));
+            #endregion
+            //================================================>>
+            #region use of int with different mathimatical operation {old way}
 
-            Console.ReadLine();
+            //int a = 12;
+            //int b = 10;
+
+            //Console.WriteLine("a + b = {0}\n a-b = {1}\n a*b = {2}", (a + b), (a - b), (a * b));
+
+            //Console.ReadLine();
+            #endregion
+            //====================================================>>
+            #region use of int with different mathimatical operation {c# 6 feature  new way}
+            //int a = 12;
+            //int b = 10;
+
+            //Console.WriteLine($"a + b = {a + b} a-b = {a - b} a*b = {a * b}");
+
+            //Console.ReadLine();
+            #endregion
+            //====================================================>>
+            #region use  of int with different mathimatical operation in a simple way
+            //int a = 1;
+            //int b = 2;
+            //int sum = a + b;
+            //int mul = a * b;
+            //int sub = a - b;
+            //Console.WriteLine(sum);
+            //Console.WriteLine(mul);
+            //Console.WriteLine(sub);
+            //Console.ReadLine();
+            #endregion
+            //=====================================================>>
+            #region use of String
+            //string address = "sialkot.pk";
+            //Console.WriteLine(address);
+            //Console.ReadLine();
+            #endregion
+            //=====================================================>>
+            #region string cancatination
+            //string fiestName = "Nisar";
+            ////string lastName = "Ahmed";
+            //string fullName = fiestName +" " + lastName;
+            //Console.WriteLine(fullName);
+            //Console.ReadLine();
+            #endregion
+            //========================================================>>
+            #region use of double 
+            //double dec = 1.4;
+            //Console.WriteLine(dec);
+            //Console.ReadLine();
+            #endregion
+            //=========================================================>>
+            #region use of double 
+            //float dec = 1.4f;
+            //Console.WriteLine(dec);
+            //Console.ReadLine();
+            #endregion
+            //=========================================================>>
+            #region getting input from user in a string
+
+            //string n;
+            //Console.WriteLine("enter your name :");
+            //n = Console.ReadLine();
+            //Console.WriteLine("your name is :{0}",n);
+            //Console.ReadLine();
+
+            #endregion
+
+            #region getting input from user in a int
+
+            //int n;
+            //Console.WriteLine("enter your age :");
+            //n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("your age is :{0}", n);
+            //Console.ReadLine();
+
+            #endregion
+
+            #region getting input from user in a string
+
+            //float ft;
+            //Console.WriteLine("enter the value of ft :");
+            //ft = float.Parse(Console.ReadLine());
+            //Console.WriteLine("value of ft is :{0}", ft);
+            //Console.ReadLine();
+
+            #endregion
         }
     }
 }
