f008861abf26eb35656c58513cf1687f0e677d8b
RAO NISAR AHMED <m.nisar@live.com>
2017-11-08 10:04:32 +0500

    Method Compleated
    All different code is writen in different region.

---
+++ 17 - Method  In C#  - C# Tutorial For Beginners - Ali Asad/Method/Method/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        #region MyRegion
+
+        static void Main(string[] args)
+        {
+            mymethod();
+            Console.ReadLine();
+
+        }
+        static void mymethod()
+        {
+            Console.WriteLine("Nisar Ahmed");
+            Console.WriteLine("Nisar Ahmed");
+            Console.WriteLine("Nisar Ahmed");
+            Console.WriteLine("Nisar Ahmed");
+            Console.WriteLine("Nisar Ahmed");
+
+        #endregion
+            #region MyRegion
+
+            //static void Main(string[] args)
+            //{
+            //    Program pr = new Program();
+            //    pr.mymethod();
+            //    Console.ReadLine();
+
+            //}
+            // void mymethod()
+            //{
+            //    Console.WriteLine("Nisar Ahmed");
+            //    Console.WriteLine("Nisar Ahmed");
+            //    Console.WriteLine("Nisar Ahmed");
+            //    Console.WriteLine("Nisar Ahmed");
+            //    Console.WriteLine("Nisar Ahmed");
+        }
+        #endregion
+        /* 
+         *  return type name (parameter_list)
+         *  {
+         *  code
+         *  }
+         */
+
+
+    }
+}
