5ad3e987641f317640bba5f87d9417ad1086951c
RAO NISAR AHMED <m.nisar@live.com>
2017-11-08 17:04:58 +0500

    Using Var and Dynamic

---
+++ 23 - Using Var and Dynamic In C#  - C# Tutorial For Beginners - Ali Asad/Using V and D/Using V and D/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_V_and_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo(55);
+            Console.ReadLine();
+        }
+        static void demo(dynamic data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
