f5f301b074bed5d66ad00f3e31195a950f471f60
RAO NISAR AHMED <m.nisar@live.com>
2017-11-08 16:21:30 +0500

    out keyword compleated

---
+++ 20 - Out keyword In C#  - C# Tutorial For Beginners - Ali Asad/Out keyword/Out keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            passbyout(out value);
+            Console.WriteLine("my value  {0}",value);
+            Console.ReadLine();
+
+        }
+
+        static void passbyout(out int myvalue)
+        {
+            myvalue = 10;
+            Console.WriteLine("my value  {0}",myvalue);
+        }
+    }
+}
