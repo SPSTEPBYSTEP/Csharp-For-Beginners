a93357548653e3b59def24395487b5f0eb4b9204
RAO NISAR AHMED <m.nisar@live.com>
2017-11-08 16:10:10 +0500

    pass by value and referance compleated

---
+++ 19 - Pass by Value and Ref  In C#  - C# Tutorial For Beginners - Ali Asad/Pass by V and R/Pass by V and R/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_by_V_and_R
+{
+    class Program
+    {
+        #region pass by value
+
+        //static void Main(string[] args)
+        //{
+        //    int value = 10;
+        //    Console.WriteLine(value);
+        //    passbyvalue(value);
+        //    Console.ReadLine();
+        //}
+
+        //static void passbyvalue(int value)
+        //{
+        //    value += 5;
+        //    Console.WriteLine(value);
+        //}
+        #endregion
+
+        #region pass by referance
+        //static void Main(string[] args)
+        //{
+        //    int myvalue = 10;
+        //    Console.WriteLine("my orgnal value {0}",myvalue);
+        //    passbyref(ref myvalue);
+        //    Console.WriteLine("my value after passing by referance {0}",myvalue);
+
+        //    Console.ReadLine();
+        //}
+
+        //static void passbyref(ref int value)
+        //{
+        //    value += 5;
+        //    Console.WriteLine("my value {0}",value);
+        //}
+        #endregion
+    }
+}
