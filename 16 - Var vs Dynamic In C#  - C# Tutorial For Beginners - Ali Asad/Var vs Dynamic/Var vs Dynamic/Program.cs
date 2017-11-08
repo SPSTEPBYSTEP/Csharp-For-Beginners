4694b389560091c57f06daacff245206721c83cf
RAO NISAR AHMED <m.nisar@live.com>
2017-11-08 09:32:31 +0500

    Var vs Dynamic compleated
    All different code is  written in different regions.

---
+++ 16 - Var vs Dynamic In C#  - C# Tutorial For Beginners - Ali Asad/Var vs Dynamic/Var vs Dynamic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_vs_Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region var as a int data type
+            //var age = 22;
+            //Console.WriteLine(age.GetType());
+            //Console.ReadLine();
+            #endregion
+
+            #region var as string data type
+            //var name = "Nisar Ahmed";
+            //Console.WriteLine(name.GetType());
+            //Console.WriteLine(name.ToUpper());
+            //Console.ReadLine();
+            #endregion
+
+            #region Dynamic
+            dynamic Age=22;
+            Console.WriteLine(Age);
+            Console.WriteLine(Age.GetType());
+           
+
+            Age = "Nisar Ahmed";
+            Console.WriteLine(Age);
+            Console.WriteLine(Age.GetType());
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
