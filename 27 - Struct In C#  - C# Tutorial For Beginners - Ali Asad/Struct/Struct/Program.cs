f5fe09802a709040210559e7907f76fac6f01f17
RAO NISAR AHMED <m.nisar@live.com>
2017-11-09 15:20:19 +0500

    struct compleated

---
+++ 27 - Struct In C#  - C# Tutorial For Beginners - Ali Asad/Struct/Struct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            stu.Age = 22;
+            stu.Name = "Nisar Ahmed";
+            Console.WriteLine(stu.Name);
+            Console.WriteLine(stu.Age);
+            Console.ReadLine();
+        }
+    }
+
+    struct Student
+    {
+       public int Age;
+        public string Name;
+    }
+}
